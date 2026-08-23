d4ef4036fb60c4033ab3d46d7409f676af742a0b
Oleksii-debug <oleksiy.az.09@gmail.com>
2026-08-23 16:22:36 +0300

    D02: add training engine package exports

---
+++ src/twelve_six/training/__init__.py
@@ -0,0 +1,25 @@
+"""Training, optimization, and numerical-safety primitives for 12-6 AI."""
+
+from .config import PrecisionMode, SchedulerKind, TrainerConfig
+from .loss import causal_lm_loss
+from .trainer import (
+    NonFiniteTrainingError,
+    StepMetrics,
+    Trainer,
+    TrainerState,
+    build_optimizer,
+    build_scheduler,
+)
+
+__all__ = [
+    "NonFiniteTrainingError",
+    "PrecisionMode",
+    "SchedulerKind",
+    "StepMetrics",
+    "Trainer",
+    "TrainerConfig",
+    "TrainerState",
+    "build_optimizer",
+    "build_scheduler",
+    "causal_lm_loss",
+]
