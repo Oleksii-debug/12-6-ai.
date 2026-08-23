925634a337a92fec015bf410f4a64dbe09c47301
Oleksii-debug <oleksiy.az.09@gmail.com>
2026-08-23 17:41:07 +0300

    D04: export vocabulary identity helpers

--- src/twelve_six/tokenization/__init__.py
+++ src/twelve_six/tokenization/__init__.py
@@ -9,19 +9,25 @@ from .base import (
 from .byte import (
     BYTE_TOKENIZER_HASH,
     BYTE_TOKENIZER_VERSION,
+    BYTE_VOCAB_HASH,
     ByteTokenizer,
     canonical_config_json,
+    canonical_vocab_json,
     tokenizer_config_hash,
+    vocab_hash,
 )
 
 __all__ = [
     "BYTE_TOKENIZER_HASH",
     "BYTE_TOKENIZER_VERSION",
+    "BYTE_VOCAB_HASH",
     "ByteTokenizer",
     "TokenizerCompatibilityError",
     "TokenizerIdentity",
     "TokenizerProtocol",
     "canonical_config_json",
+    "canonical_vocab_json",
     "require_tokenizer_identity",
     "tokenizer_config_hash",
+    "vocab_hash",
 ]
