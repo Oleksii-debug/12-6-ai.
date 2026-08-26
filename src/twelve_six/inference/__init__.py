f1d1ec5878f7c84808221ffe0bce076fefef1dcb
Oleksii-debug <oleksiy.az.09@gmail.com>
2026-08-26 21:23:06 +0300

    inference: export first-party API

--- src/twelve_six/inference/__init__.py
+++ src/twelve_six/inference/__init__.py
@@ -1,4 +1,19 @@
+from .api import (
+    FirstPartyInference,
+    load_first_party_inference,
+    load_random_init_inference,
+)
 from .contracts import GenerationConfig, GenerationResult, InferenceBackend
-from .generation import generate
+from .generation import CacheMode, generate, generate_token_ids
 
-__all__ = ["GenerationConfig", "GenerationResult", "InferenceBackend", "generate"]
+__all__ = [
+    "CacheMode",
+    "FirstPartyInference",
+    "GenerationConfig",
+    "GenerationResult",
+    "InferenceBackend",
+    "generate",
+    "generate_token_ids",
+    "load_first_party_inference",
+    "load_random_init_inference",
+]
