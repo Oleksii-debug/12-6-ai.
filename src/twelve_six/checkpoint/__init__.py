acb47bd71ba73b5bcd8ee86aa3c877baaaed303b
Oleksii-debug <oleksiy.az.09@gmail.com>
2026-08-26 22:20:06 +0300

    d05: route public checkpoint loads through hardening

--- src/twelve_six/checkpoint/__init__.py
+++ src/twelve_six/checkpoint/__init__.py
@@ -12,12 +12,14 @@ from .core import (
     detect_git_sha,
     environment_snapshot,
     hash_json,
-    load_checkpoint,
-    load_verified_checkpoint,
-    prepare_checkpoint_load,
     restore_rng_state,
     save_checkpoint,
     sha256_file,
+)
+from .hardening import (
+    load_checkpoint,
+    load_verified_checkpoint,
+    prepare_checkpoint_load,
     verify_checkpoint,
 )
 from .hf_export import export_hf_directory
