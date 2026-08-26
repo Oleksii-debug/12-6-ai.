0267a4a3a7723959b174342186584506a0536c7f
Oleksii-debug <oleksiy.az.09@gmail.com>
2026-08-26 17:58:19 +0300

    RUNTIME-348 export maintained 20M inference API

--- src/twelve_six/inference/__init__.py
+++ src/twelve_six/inference/__init__.py
@@ -1,4 +1,25 @@
 from .contracts import GenerationConfig, GenerationResult, InferenceBackend
 from .generation import generate
+from .twenty_m import (
+    TWENTY_M_MAX_PARAMETERS,
+    TWENTY_M_MIN_PARAMETERS,
+    TWENTY_M_TARGET_PARAMETERS,
+    TwentyMInference,
+    load_20m_model_spec,
+    open_20m_inference,
+    validate_20m_spec,
+)
 
-__all__ = ["GenerationConfig", "GenerationResult", "InferenceBackend", "generate"]
+__all__ = [
+    "GenerationConfig",
+    "GenerationResult",
+    "InferenceBackend",
+    "TWENTY_M_MAX_PARAMETERS",
+    "TWENTY_M_MIN_PARAMETERS",
+    "TWENTY_M_TARGET_PARAMETERS",
+    "TwentyMInference",
+    "generate",
+    "load_20m_model_spec",
+    "open_20m_inference",
+    "validate_20m_spec",
+]
